b1f539047e69d5e6b95b8537ec4026a08ac50863
G3rsonBR <gersons.br13@gmail.com>
2024-03-17 16:37:43 -0300

    Projeto finalizado e responsivo

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="center"> Portfólio Responsivo </h1>
+
+<p align="left">
+Site para atividade prática do Instituto Proa! É uma versão modificada de um tutorial de Portfólio. <br/> Aproveitei dessa oportunidade de aprimorar meus conhecimentos e criar com HTML, CSS e Javascript meu próprio Portfólio.  
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto E-commerce" src=".github/image.png" width="100%">
+</p> 
---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links")
+  const icon = document.querySelector(".hamburger-icon")
+  menu.classList.toggle("open")
+  icon.classList.toggle("open")
+}
+
+
+// Script do carousel:
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= itemWidth + padding
+})
+
+next.addEventListener('click',()=>{
+  list.scrollLeft += itemWidth + padding
+})
